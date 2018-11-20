9e670967931e5fb7dabc909c988ecc30c924253c
Kangb0tmang <jon.kang.jd@gmail.com>
2018-11-20 21:22:33 +1100

    input tests

--- tests/invoice_test.rb
+++ tests/invoice_test.rb
@@ -1,9 +1,22 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
-require_relative 'invoice'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
+require_relative 'invoice'
+
 class InvoiceTest < MiniTest::Test
+
+  def test_is_number
+    input = 1
+    assert_equal true, input.is_a?(Integer)
+  end
+
+  def test_is_not_number
+    input = "1"
+    input = "watermelon"
+    assert_equal false, input.is_a?(Integer)
+    assert_equal false, input.is_a?(Integer)
+  end
 
 end
