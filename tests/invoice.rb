207100343319c7c9180beb507c4fc950a180c773
Kangb0tmang <jon.kang.jd@gmail.com>
2018-11-21 18:07:43 +1100

    output invoice and totals

--- tests/invoice.rb
+++ tests/invoice.rb
@@ -30,6 +30,41 @@ class Invoice
     end
   end
 
+  def calculate_packs(pricing)
+    if pricing[:large][:count] > 0
+      puts "#{pricing[:large][:count]}x #{pricing[:large][:pack]} pack @ $#{pricing[:large][:price]}"
+    end
+
+    if pricing[:medium][:count] > 0
+      puts "#{pricing[:medium][:count]}x #{pricing[:medium][:pack]} pack @ $#{pricing[:medium][:price]}"
+    end
+
+    if pricing[:small][:count] > 0
+      puts "#{pricing[:small][:count]}x #{pricing[:small][:pack]} pack @ $#{pricing[:small][:price]}"
+    end
+  end
+
+  def calculate_subtotal(pricing, quantity, fruit)
+    if fruit == "watermelons"
+      large = pricing[:large][:count] * pricing[:large][:price]
+      small = pricing[:small][:count] * pricing[:small][:price]
+      @watermelon_subtotal = large + small
+      puts "#{quantity} #{fruit.capitalize} $#{@watermelon_subtotal}"
+    elsif fruit == "pineapples"
+      large = pricing[:large][:count] * pricing[:large][:price]
+      medium = pricing[:medium][:count] * pricing[:medium][:price]
+      small = pricing[:small][:count] * pricing[:small][:price]
+      @pineapple_subtotal = large + medium + small
+      puts "#{quantity} #{fruit.capitalize} $#{@pineapple_subtotal}"
+    elsif fruit == "rockmelons"
+      large = pricing[:large][:count] * pricing[:large][:price]
+      medium = pricing[:medium][:count] * pricing[:medium][:price]
+      small = pricing[:small][:count] * pricing[:small][:price]
+      @rockmelon_subtotal = large + medium + small
+      puts "#{quantity} #{fruit.capitalize} $#{@pineapple_subtotal}"
+    end
+  end
+
   def calculate_watermelons(quantity)
     remainder = quantity
     large_count = small_count = 0
@@ -48,11 +83,24 @@ class Invoice
       end
     end
 
-    large_packs = large_count * 8.99
-    small_packs = small_count * 6.99
-    @watermelon_subtotal = large_packs + small_packs
+    pricing = {
+      :large => {
+        :count => large_count,
+        :pack => large,
+        :price => 8.99
+      },
+      :medium => {
+        :count => 0
+      },
+      :small => {
+        :count => small_count,
+        :pack => small,
+        :price => 6.99
+      }
+    }
 
-    puts "#{quantity} Watermelons $#{large_packs + small_packs}"
+    calculate_subtotal(pricing, quantity, "watermelons")
+    calculate_packs(pricing)
   end
 
   def calculate_pineapples(quantity)
@@ -77,12 +125,26 @@ class Invoice
       end
     end
 
-    large_packs = large_count * 24.95
-    medium_packs = medium_count * 16.95
-    small_packs = small_count * 9.95
-    @pineapple_subtotal = large_packs + medium_packs + small_packs
+    pricing = {
+      :large => {
+        :count => large_count,
+        :pack => large,
+        :price => 24.95
+      },
+      :medium => {
+        :count => medium_count,
+        :pack => medium,
+        :price => 16.95
+      },
+      :small => {
+        :count => small_count,
+        :pack => small,
+        :price => 9.95
+      }
+    }
 
-    puts "#{quantity} Pineapples $#{large_packs + medium_packs + small_packs}"
+    calculate_subtotal(pricing, quantity, "pineapples")
+    calculate_packs(pricing)
   end
 
   def calculate_rockmelons(quantity)
@@ -107,12 +169,26 @@ class Invoice
       end
     end
 
-    large_packs = large_count * 16.99
-    medium_packs = medium_count * 9.95
-    small_packs = small_count * 5.95
-    @rockmelon_subtotal = large_packs + medium_packs + small_packs
+    pricing = {
+      :large => {
+        :count => large_count,
+        :pack => large,
+        :price => 16.99
+      },
+      :medium => {
+        :count => medium_count,
+        :pack => medium,
+        :price => 9.95
+      },
+      :small => {
+        :count => small_count,
+        :pack => small,
+        :price => 5.95
+      }
+    }
 
-    puts "#{quantity} Rockmelons $#{large_packs + medium_packs + small_packs}"
+    calculate_subtotal(pricing, quantity, "rockmelons")
+    calculate_packs(pricing)
   end
 
 end
