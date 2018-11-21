d18575778c6ae0d8f78949d607f72f3c7771ce2d
Kangb0tmang <jon.kang.jd@gmail.com>
2018-11-21 16:03:28 +1100

    calculate_fruit function, added subtotals

--- tests/invoice.rb
+++ tests/invoice.rb
@@ -1,17 +1,26 @@
 class Invoice
 
   def initialize
+    @watermelon_subtotal
+    @pineapple_subtotal
   end
 
-  def get_input(input)
-    watermelon_small_pack = 3
-
-    while input < watermelon_small_pack
-      puts "Please enter in 3 or more"
+  def get_input(input, small_pack, fruit)
+    while input < small_pack
+      puts "Please enter 3 or more"
       input = gets.chomp.to_i
     end
 
-    calculate_watermelons(input)
+    calculate_fruit(input, fruit)
+  end
+
+  def calculate_fruit(input, fruit)
+    if fruit == "watermelons"
+      calculate_watermelons(input)
+    elsif fruit == "pineapples"
+      calculate_pineapples(input)
+    else
+    end
   end
 
   def calculate_watermelons(quantity)
@@ -34,8 +43,39 @@ class Invoice
 
     large_packs = large_count * 8.99
     small_packs = small_count * 6.99
+    @watermelon_subtotal = large_packs + small_packs
 
     puts "#{quantity} Watermelons $#{large_packs + small_packs}"
+  end
+
+  def calculate_pineapples(quantity)
+    remainder = quantity
+    large_count = medium_count = small_count = 0
+    large = 8
+    medium = 5
+    small = 2
+
+    while remainder > 0
+      if remainder >= large
+        large_count += 1
+        remainder = remainder - large
+      elsif remainder.between?(medium, (large - 1))
+        medium_count += 1
+        remainder = remainder - medium
+      elsif remainder.between?(small, (medium - 1))
+        small_count += 1
+        remainder = remainder - small
+      elsif remainder < small
+        break
+      end
+    end
+
+    large_packs = large_count * 24.95
+    medium_packs = medium_count * 16.95
+    small_packs = small_count * 9.95
+    @pineapple_subtotal = large_packs + medium_packs + small_packs
+
+    puts "#{quantity} Pineapples $#{large_packs + medium_packs + small_packs}"
   end
 
 end
@@ -43,5 +83,12 @@ end
 puts "How many watermelons? (must be 3 or more)"
 watermelons = gets.chomp.to_i
 
+puts "How many pineapples? (must be 2 or more)"
+pineapples = gets.chomp.to_i
+
+watermelon_small = rockmelon_small = 3
+pineapple_small = 2
+
 order = Invoice.new
-order.get_input(watermelons)
+order.get_input(watermelons, watermelon_small, "watermelons")
+order.get_input(pineapples, pineapple_small, "pineapples")
