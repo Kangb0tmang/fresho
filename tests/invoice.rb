35913c84e804d942932431111080283056dbd3f6
Kangb0tmang <jon.kang.jd@gmail.com>
2018-11-21 14:35:20 +1100

    calculate price of watermelons

--- tests/invoice.rb
+++ tests/invoice.rb
@@ -1,20 +1,34 @@
-# require 'pry'
-
 class Invoice
 
   def initialize
   end
 
-  def watermelons(quantity)
-    puts "#{quantity} Watermelons $#{quantity * 8.99}"
+  def calculcate_watermelons(quantity)
+    large_count = small_count = 0
+    large = 5
+    small = 3
+
+    while quantity > 0
+      if quantity >= large
+        large_count += 1
+        quantity = quantity - large
+      elsif quantity.between?(small, (large - 1))
+        small_count += 1
+        quantity = quantity - small
+      elsif quantity < small
+        break
+      end
+    end
+
+    large_packs = large_count * 8.99
+    small_packs = small_count * 6.99
+
+    puts "#{quantity} Watermelons $#{large_packs + small_packs}"
   end
 
 end
 
-puts "How many watermelons?"
-
+puts "How many watermelons? (must be 3 or more)"
 input = gets.chomp.to_i
 order = Invoice.new
-order.watermelons(input)
-
-# binding.pry
+order.calculcate_watermelons(input)
