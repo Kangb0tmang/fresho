e5cc0b921a4798a465b8d3b8b2ef965389d48719
Kangb0tmang <jon.kang.jd@gmail.com>
2018-11-21 15:24:39 +1100

    accounts for minimum quantity

--- tests/invoice.rb
+++ tests/invoice.rb
@@ -3,19 +3,31 @@ class Invoice
   def initialize
   end
 
-  def calculcate_watermelons(quantity)
+  def get_input(input)
+    watermelon_small_pack = 3
+
+    while input < watermelon_small_pack
+      puts "Please enter in 3 or more"
+      input = gets.chomp.to_i
+    end
+
+    calculate_watermelons(input)
+  end
+
+  def calculate_watermelons(quantity)
+    remainder = quantity
     large_count = small_count = 0
     large = 5
     small = 3
 
-    while quantity > 0
-      if quantity >= large
+    while remainder > 0
+      if remainder >= large
         large_count += 1
-        quantity = quantity - large
-      elsif quantity.between?(small, (large - 1))
+        remainder = remainder - large
+      elsif remainder.between?(small, (large - 1))
         small_count += 1
-        quantity = quantity - small
-      elsif quantity < small
+        remainder = remainder - small
+      elsif remainder < small
         break
       end
     end
@@ -29,6 +41,7 @@ class Invoice
 end
 
 puts "How many watermelons? (must be 3 or more)"
-input = gets.chomp.to_i
+watermelons = gets.chomp.to_i
+
 order = Invoice.new
-order.calculcate_watermelons(input)
+order.get_input(watermelons)
